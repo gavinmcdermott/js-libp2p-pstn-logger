482dc44c0cbe3fdba97a5c8509bad9bde7da0a72
Gavin McDermott <gavinmcdermott@gmail.com>
2016-09-29 00:26:20 -0700

    add initial logger tests

--- src/logger.js
+++ src/logger.js
@@ -1,25 +1,30 @@
 'use strict'
 
 const EE = require('events').EventEmitter
-const TestnetNode = require('libp2p-pstn-node')
+const TestNode = require('libp2p-pstn-node')
 const R = require('ramda')
 
 const { log } = require('./config')
 const { LoggerError } = require('./errors')
 
-class Logger {
-  constructor(testnetNode, pubsub) {
+module.exports = class Logger {
+  constructor(testNode, pubsub) {
     if (!(this instanceof Logger)) {
-      return new Logger(testnetNode, pubsub)
+      return new Logger(testNode, pubsub)
     }
 
-    if (!(testnetNode instanceof TestnetNode)) {
-      throw new LoggerError('Expect <testnetNode> to be instance of libp2p-pstn-node')
+    if (!(testNode instanceof TestNode)) {
+      throw new LoggerError('Expect <testNode> to be instance of libp2p-pstn-node')
     }
 
     if (R.isNil(pubsub)) {
       throw new LoggerError('Missing <pubsub>')
     }
 
+    // Add an eventEmitter to the instance
+    EE.call(this)
+
+    this.node = testNode
+    this.pubsub = pubsub
   }
 }
--- test/logger.spec.js
+++ test/logger.spec.js
@@ -1,19 +1,36 @@
 'use strict'
 
 const expect = require('chai').expect
-// const PSG = require('libp2p-floodsub')
-const FloodSub = require('libp2p-floodsub')
+// Note: require('libp2p-floodsub') throws: Cannot find module 'libp2p-floodsub'
+const PSG = require('./../node_modules/libp2p-floodsub/src/index')
+const TestNode = require('libp2p-pstn-node')
 // const R = require('ramda')
+const keys = require('./fixtures/keys').keys
 const Logger = require('./../src/index')
 
 describe('Logger', () => {
   let logger
+  const testNode = new TestNode({ id: keys[0] })
 
   describe('constructor', () => {
-    it('fails: missing valid Node', () => {
+    it('fails: missing Node', () => {
       const thrower = () => new Logger()
       expect(thrower).to.throw
     })
-  })
 
+    it('fails: invalid Node', () => {
+      const thrower = () => new Logger({})
+      expect(thrower).to.throw
+    })
+
+    it('fails: missing pubsub', () => {
+      const thrower = () => new Logger(testNode)
+      expect(thrower).to.throw
+    })
+
+    it('success', () => {
+      logger = new Logger(testNode, PSG)
+      expect(logger instanceof Logger).to.be.true
+    })
+  })
 })
