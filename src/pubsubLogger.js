a641a67dd6e15c489edfd3ca058490d883a0e2f2
Gavin McDermott <gavinmcdermott@gmail.com>
2016-09-29 17:12:49 -0700

    add emit test for single node and more test groundwork

--- src/pubsubLogger.js
+++ src/pubsubLogger.js
@@ -4,28 +4,19 @@ const R = require('ramda')
 const { pubsubLogger, LOGGER_EVENT_BASE } = require('./config')
 
 module.exports = (logger) => {
-  const id = logger.id
   const ps = logger.pubsub
-
-
-
-
 
   const decorate = function (fn, event) {
     return (...args) => {
       const data = {
-        id,
+        loggerId: logger.id,
         event,
         args
       }
-      // pubsubLogger(fn)
-      // pubsubLogger(name)
-      // pubsubLogger(args)
       logger.emit(LOGGER_EVENT_BASE, data)
       return fn.apply(null, args)
     }
   }
-
 
   ps.subscribe = decorate(ps.subscribe, 'subscribe')
 }
--- test/logger.spec.js
+++ test/logger.spec.js
@@ -10,23 +10,11 @@ const parallel = require('run-parallel')
 const keys = require('./fixtures/keys').keys
 const Logger = require('./../src/index')
 
+const mapIndexed = R.addIndex(R.map)
+
 describe('Logger', () => {
-  let logger
-  let nodes = R.range(0, 3)
-
-  before(() => {
-    R.forEach((idx) => {
-      let node = new TestNode({ id: keys[idx], portOffset: idx })
-      nodes[idx] = node
-    }, nodes)
-  })
-
   describe('constructor', () => {
-    let nodeA
-
-    before(() => {
-      nodeA = R.head(nodes)
-    })
+    const validNode = new TestNode({ id: keys[0], portOffset: 0 })
 
     it('fails: missing Node', () => {
       const thrower = () => new Logger()
@@ -39,15 +27,71 @@ describe('Logger', () => {
     })
 
     it('fails: missing pubsub', () => {
-      const thrower = () => new Logger(nodeA)
+      const thrower = () => new Logger(validNode)
       expect(thrower).to.throw
     })
 
     it('success', () => {
-      logger = new Logger(nodeA, PS)
+      const logger = new Logger(validNode, PS)
       expect(logger instanceof Logger).to.be.true
       expect(logger.node instanceof TestNode).to.be.true
       expect(logger.pubsub instanceof PS).to.be.true
+    })
+  })
+
+  describe('one node', () => {
+    let loggerA
+    let loggers = R.range(0, 1)
+
+    const topicA = 'AAA'
+
+    before(() => {
+      const startFns = mapIndexed((n, idx) => {
+        let node = new TestNode({ id: keys[idx], portOffset: idx })
+        loggers[idx] = new Logger(node, PS)
+        return node.start
+      }, loggers)
+
+      loggerA = loggers[0]
+
+      return Promise.all(startFns)
+    })
+
+    after(() => {
+      const stopFns = R.map((logger) => {
+        return logger.node.libp2p.stop
+      },loggers)
+      return Promise.all(stopFns)
+    })
+
+    describe('emit', () => {
+      it('"subscribe" events', () => {
+        let counter = 0
+
+        loggerA.on('data', (data) => {
+          const loggerId = data.loggerId
+          const event = data.event
+          const topic = data.args[0]
+
+          expect(R.equals(loggerId, loggerA.id)).to.be.true
+          expect(R.equals(event, 'subscribe')).to.be.true
+          expect(R.equals(topic, topicA)).to.be.true
+
+          counter++
+        })
+
+        expect(counter).to.equal(0)
+
+        loggerA.pubsub.subscribe(topicA)
+
+        expect(counter).to.equal(1)
+      })
+
+
+
+
+
+
     })
   })
 })
